44f425efc11c910bc33bed90a5e96aa7d2cd8fe8
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-04-12 17:43:17 +0545

    project initiated

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <v-container>
+        <v-row justify="center" align="center">
+             <h3>3D PAGES</h3>
+        </v-row>
+    </v-container>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage'
+}
+</script>
